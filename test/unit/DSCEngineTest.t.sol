b34a28ad35d35dd6f10a98248140c9547c4226a3
zntb <zntbdev@proton.me>
2024-09-19 15:59:57 +0300

    Create the depositAndMint function

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import  {Test} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
-import { ERC20Mock } from "../mocks/ERC20Mock.sol";
+import {ERC20Mock} from "../mocks/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC deployer;
@@ -22,7 +22,7 @@ contract DSCEngineTest is Test {
 
     function setup() public {
         deployer = new DeployDSC();
-        (dsc, dscEngine, config ) = deployer.run();
+        (dsc, dscEngine, config) = deployer.run();
         (ethUsdPriceFeed,, weth,,) = config.activeNetworkConfig();
 
         ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
